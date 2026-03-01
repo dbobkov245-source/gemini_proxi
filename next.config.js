8d9f72bf9a46b1e385987b958b5638220aacc0d3
dbobkov245-source <dbobkov245@gmail.com>
2026-03-01 13:07:57 +0300

    chore: update CORS config for /v1beta path

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: '/v1beta/:path*',
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'POST, OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, x-secret' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
         ],
       },
     ]
